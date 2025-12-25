38848c57b82a456e6c7df7864a80d901e7d470a9
cgkim <cgkim80@gmail.com>
2025-12-26 01:02:40 +0900

    dataframe exercise

--- script3.R
+++ script3.R
@@ -25,3 +25,63 @@ qplot(data=mpg, x =drv, y= hwy, geom ='boxplot', color=drv)
 ?qplot
 
 mpg
+#데이터 분석의 가치는 어떤 현상이 조건에 따라 달라진다.라는점이다.
+# 특정날씨에 어떤 음식이 더 많이 팔린다. 같은것이다.
+
+
+# =============================
+# 데이터 프레임
+# =============================
+english <- c(90,80,60,70)
+math <- c(50,60,100,20)
+df_midterm <-data.frame(english,math)
+
+df_midterm
+mean(df_midterm$english)
+
+df_midterm2 <-data.frame(english = c(1,2,4,5),
+                         math = c(44,44,2,2))
+df_midterm2
+
+
+# =============================
+# 엑셀파일 불러오기
+# =============================
+
+install.packages("readxl")
+library(readxl)
+df_exam <- read_excel("excel_exam.xlsx")
+df_exam
+mean(df_exam$math)
+mean(df_exam$english)
+mean(df_exam$science)
+
+df_exam2 <- read_excel("excel_exam.xlsx", col_names=F)
+df_exam2
+
+
+df_exam3 <- read_excel("excel_exam.xlsx", sheet=2)
+df_exam3
+## csv의 경우 다름 
+## read.csv("file_name.csv")
+## read.csv("file_name.csv", header=F)
+
+# ============================
+# data frame to csv
+# ============================
+
+df_midterm_csv <- data.frame(english =c(1,2,4,5), math = c(3,4,5,5))
+df_midterm_csv
+write.csv(df_midterm_csv, file="df_midterm_csv.csv")
+
+
+# ================================
+# RDS : r사용하는 데이터 저장포멧
+# r전용파일 데이터로 속도가 빠르고 용량이 작음
+# 일반 메모장에서는 내용이 안보임
+# ================================
+saveRDS(df_midterm2, file="df_midterm2.rds")
+rm(df_midterm2)
+df_midterm2
+df_midterm2 <-readRDS("df_midterm2.rds")
+df_midterm2
