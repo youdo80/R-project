70d5b036f985cf9d00759ff348c0acd77da5b790
cgkim <cgkim80@gmail.com>
2025-12-28 21:39:20 +0900

    day27

--- script3.R
+++ script3.R
@@ -85,3 +85,50 @@ rm(df_midterm2)
 df_midterm2
 df_midterm2 <-readRDS("df_midterm2.rds")
 df_midterm2
+
+
+# =======================================
+# 데이터 파악하기
+# =======================================
+df_iris <- readRDS("iris.rds")
+head(df_iris)
+head(df_iris,10)
+tail(df_iris)
+
+## 엑셀과 같이 뷰어 창이 나와서 데이터 확인가능, 첫 V 대문자
+View(df_iris)
+
+dim(df_iris) #행, 열 출력
+
+str(df_iris) #데이터 속성파악
+
+summary(df_iris)
+
+library(ggplot2)
+
+mpg <- as.data.frame(ggplot2::mpg)
+head(mpg)
+tail(mpg)
+dim(mpg)
+str(mpg)
+summary(mpg)
+
+?mpg
+
+
+# ----------------------
+# 변수명 바꾸기
+# ----------------------
+
+df_raw <-data.frame(var1 = c(1,2,4), var2 = c(2,4,2))
+df_raw
+
+# 패키지 설치
+install.packages("dplyr")
+library(dplyr)
+df_new <-df_raw        
+df_new
+df_new <-rename(df_new, v2  = var2) #새변수명 = 기존변수명
+df_new
+df_new <- rename(df_new, v1 = var1)
+df_new
